3ed8bb5e7eb841614b0c05269e5f22f130fe840c
Elena Savina <savina1289@yandex.ru>
2022-06-21 21:34:03 +0300

    Added calculator

--- src/main/kotlin/ru/netology/transfers.kt
+++ src/main/kotlin/ru/netology/transfers.kt
@@ -1,2 +1,11 @@
 package ru.netology
 
+fun main() {
+    val minFee = 35_00
+    val amount = 5000_00
+
+    val transferFee = (amount * 0.0075).toInt()
+    val resultFee = if (transferFee > minFee) transferFee else minFee
+
+    println("При переводе суммы ${amount / 100} руб. комиссия составит ${resultFee / 100} руб. ${resultFee % 100} коп.")
+}
